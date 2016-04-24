9e57a1bbe74af59880190097904ed709c2e2f54c
Takahiro Tsuchiya <regist.only.account+github@gmail.com>
2016-04-24 21:27:26 +0900

    Initial commit

---
+++ src/com/sample/SampleClass.java
@@ -0,0 +1,47 @@
+package com.sample;
+
+public class SampleClass {
+
+	public boolean method0001(int a, int b) {
+
+		boolean flag;
+
+		if (a == b) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+
+		return flag;
+	}
+
+	public boolean method0002(int a, int b) {
+
+		boolean flag = false;
+
+		if (a == b) {
+			flag = true;
+		}
+
+		return flag;
+	}
+
+	public int method0003(int a) {
+
+		int b = 1;
+
+		return b + a;
+	}
+
+	public int method0004(int a, int b) {
+
+		return a + b;
+	}
+
+	public int method0005(int a) {
+
+		int b = Integer.MAX_VALUE;
+
+		return b + a;
+	}
+}
---
+++ test/com/sample/SampleClassTest.java
@@ -0,0 +1,109 @@
+package com.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.sample.SampleClass;
+
+import junit.framework.TestCase;
+
+@RunWith(JUnit4.class)
+public class SampleClassTest extends TestCase {
+
+	SampleClass testClass = new SampleClass();
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+
+		System.err.println("[Integer.MIN_VALUE]" + Integer.MIN_VALUE);
+		System.err.println("[Integer.MAX_VALUE]" + Integer.MAX_VALUE);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+
+	@Test
+	public void method0001Test0001() {
+
+		assertEquals("Error ", true, testClass.method0001(1, 1));
+	}
+
+	@Test
+	public void method0001Test0002() {
+
+		assertEquals("Error ", false, testClass.method0001(1, 2));
+	}
+
+	@Test
+	public void method0002Test0001() {
+
+		assertEquals("Error ", true, testClass.method0002(1, 1));
+	}
+
+	@Test
+	public void method0002Test0002() {
+
+		assertEquals("Error ", false, testClass.method0002(1, 2));
+	}
+
+	@Test
+	public void method0003Test0001() {
+
+		assertEquals("Error ", Integer.MIN_VALUE + 1, testClass.method0003(Integer.MIN_VALUE));
+	}
+
+	@Test
+	public void method0003Test0002() {
+
+		assertEquals("Error ", 1, testClass.method0003(0));
+	}
+
+	@Test
+	public void method0003Test0003() {
+
+		assertEquals("Error ", Integer.MIN_VALUE, testClass.method0003(Integer.MAX_VALUE));
+	}
+
+	@Test
+	public void method0004Test0001() {
+
+		assertEquals("Error ", 0, testClass.method0004(Integer.MIN_VALUE, Integer.MIN_VALUE));
+	}
+
+	@Test
+	public void method0004Test0002() {
+
+		assertEquals("Error ", -1, testClass.method0004(Integer.MIN_VALUE, Integer.MAX_VALUE));
+	}
+
+	@Test
+	public void method0004Test0003() {
+
+		assertEquals("Error ", -1, testClass.method0004(Integer.MAX_VALUE, Integer.MIN_VALUE));
+	}
+
+	@Test
+	public void method0004Test0004() {
+
+		assertEquals("Error ", -2, testClass.method0004(Integer.MAX_VALUE, Integer.MAX_VALUE));
+	}
+
+	@Test
+	public void method0005Test0001() {
+
+		assertEquals("Error ", -1, testClass.method0005(Integer.MIN_VALUE));
+	}
+
+	@Test
+	public void method0005Test0002() {
+
+		assertEquals("Error ", -2, testClass.method0005(Integer.MAX_VALUE));
+	}
+
+}
